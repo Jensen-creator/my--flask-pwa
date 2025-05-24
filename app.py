886e6d5c876787554d414f2932ad69235ef6103a
Jensen <Jensenzlew@gmail.com>
2025-05-24 11:25:20 +0800

    Initial commit

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template, request, redirect
+
+app = Flask(__name__)
+
+# Store tasks in memory (for now)
+tasks = []
+
+@app.route('/')
+def index():
+    return render_template('index.html', tasks=tasks)
+
+@app.route('/add', methods=['POST'])
+def add():
+    task = request.form.get('task')
+    if task:
+        tasks.append(task)
+    return redirect('/')
+
+if __name__ == '__main__':
+    app.run(debug=True)
